81f3bc0f5f552b6e918314d28799b3e60d28da05
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-29 00:15:56 +0900

    [1-started] feat: add solution fot price.js

---
+++ 1-started/math-methods/price.js
@@ -0,0 +1,8 @@
+// https://dingco.notion.site/DM-bd60f11319424d1394c0f20ac975fbf0
+
+function solution(prices) {
+  return Math.min(...prices)
+}
+
+solution([20000, 113000, 67000, 40500, 99000]) // 20000
+solution([19900, 54200, 43000]) // 19900
