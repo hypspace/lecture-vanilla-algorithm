1049365566e06664f605a06ed49f59749dfa7278
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-28 20:51:55 +0900

    [1-started] feat: add solution fot password.js

---
+++ 1-started/operators/password.js
@@ -0,0 +1,8 @@
+// https://dingco.notion.site/8572eaee34964383a491955190168d11
+
+function solution(birth, date) {
+  return `비밀번호는 ${birth + date}입니다.`
+}
+
+solution(900501, 2021) // 비밀번호는 902522입니다.
+solution(930219, 2020) // 비밀번호는 932239입니다.
