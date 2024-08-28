378e3b7d7562a4b15f4b6bb67ec3608a22382338
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-28 21:45:26 +0900

    [1-started] feat: add solution fot name.js

---
+++ 1-started/conditionals/name.js
@@ -0,0 +1,8 @@
+// https://dingco.notion.site/a7dfc17175864109b72a66754776dc81
+
+function solution(name) {
+  return name === '나사장' ? 'code100' : 'code50'
+}
+
+solution('나사장') // code100
+solution('나임원') // code50
