1eb199c08a1afcba477d8d4bb3c28cb0c127fac8
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-28 19:37:06 +0900

    [1-started] feat: add solution fot let.js

---
+++ 1-started/variables/let.js
@@ -0,0 +1,10 @@
+// https://dingco.notion.site/84e5aa8a155b4f67a10c1d4c6cdab1b2
+
+function solution(a, b) {
+  let company = a
+  company = b
+  return company
+}
+
+solution('캠프코드', '코드캠프') // "코드캠프"
+solution('코딩', '딩코') // "딩코"
